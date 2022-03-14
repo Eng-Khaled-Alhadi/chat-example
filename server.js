49dcbe7c3f2bed65716fc576da226f6ba6cb35ad
MAC <85607311+Eng-Khaled-Alhadi@users.noreply.github.com>
2022-03-14 15:25:37 +0300

    a

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-
+app.get('/', (req, res) => {
+    console.log("1")
+    res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', (socket) => {
     console.log("new")
